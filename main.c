a846a5e2e945684916d2e744cf30b14124e6f78c
Vianney Veremme <10519369+Vianpyro@users.noreply.github.com>
2024-10-08 16:03:29 -0400

    Update main.c

--- main.c
+++ main.c
@@ -2,18 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    int *leak = (int *)malloc(10 * sizeof(int));  // Allocate memory
-
-    // Simulate usage of allocated memory
-    for (int i = 0; i < 10; i++) {
-        leak[i] = i;
-        printf("%d ", leak[i]);
-    }
-
-    printf("\n");
-
-    // Intentionally forget to free the allocated memory to create a memory leak
-    // free(leak);  // This line is missing, causing the memory leak
+    printf("Hello World\n");
 
     return 0;
 }
